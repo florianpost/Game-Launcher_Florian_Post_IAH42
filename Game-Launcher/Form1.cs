0cfe3265c17ec69c72de207e70bc0f5209405528
florian_post <florian_post@me.com>
2017-06-16 14:55:15 +0200

    1. start

--- Game-Launcher/Form1.cs
+++ Game-Launcher/Form1.cs
@@ -16,5 +16,10 @@ namespace Game_Launcher
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //start
+        }
     }
 }
