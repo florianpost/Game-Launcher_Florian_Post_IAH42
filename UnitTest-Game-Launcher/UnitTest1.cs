1dd0910901e33bd51e9be9c7eca79784f15299f3
florian_post <florian_post@DESKTOP-A33VNFS>
2017-06-16 14:50:28 +0200

    Add project files.

---
+++ UnitTest-Game-Launcher/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest_Game_Launcher
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
